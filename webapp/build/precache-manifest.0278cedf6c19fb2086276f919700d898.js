df8e836aea4baf2b72907248a19379ad0d52fdb4
tbach <thbach@gmail.com>
2018-11-21 22:43:25 +0000

    added build

---
+++ webapp/build/precache-manifest.0278cedf6c19fb2086276f919700d898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bd67ae2e4063e7115cb4",
+    "url": "/static/js/main.bd67ae2e.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "bd67ae2e4063e7115cb4",
+    "url": "/static/css/main.67a6b3f2.chunk.css"
+  },
+  {
+    "revision": "6033ce27ce045a4a6c697e476bc7ab6e",
+    "url": "/index.html"
+  }
+];
